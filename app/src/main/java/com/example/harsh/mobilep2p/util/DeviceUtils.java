4fa2b9118e27e38d6117aab49715ff435e1e3516
Harsh Shah <harsh.shah.jayesh@gmail.com>
2017-11-13 19:08:43 +0530

    Fixed hotspot issue

--- app/src/main/java/com/example/harsh/mobilep2p/util/DeviceUtils.java
+++ app/src/main/java/com/example/harsh/mobilep2p/util/DeviceUtils.java
@@ -11,9 +11,13 @@ import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
 /**
@@ -25,6 +29,7 @@ public class DeviceUtils {
     private static final String UPLOAD_DIRECTORY = "/Upload/";
     private static final String TAG = "DeviceUtils";
 
+    /*
     // TODO: Get broadcast address in different way
     public InetAddress getBroadcastAddress(Context context) throws IOException {
         WifiManager wifi = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
@@ -36,9 +41,28 @@ public class DeviceUtils {
         for (int k = 0; k < 4; k++)
             quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
         return InetAddress.getByAddress(quads);
+    }*/
+
+    public InetAddress getBroadcastAddress(Context context) {
+        InetAddress found_bcast_address = null;
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        try {
+            Enumeration<NetworkInterface> niEnum = NetworkInterface.getNetworkInterfaces();
+            while (niEnum.hasMoreElements()) {
+                NetworkInterface ni = niEnum.nextElement();
+                if (!ni.isLoopback()) {
+                    for (InterfaceAddress interfaceAddress : ni.getInterfaceAddresses()) {
+                        found_bcast_address = interfaceAddress.getBroadcast();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return found_bcast_address;
     }
 
-    public InetAddress getDeviceIPAddress(Context context) {
+    /*public InetAddress getDeviceIPAddress(Context context) {
         WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         int ipAddressInt = wifiManager.getConnectionInfo().getIpAddress();
 
@@ -56,15 +80,35 @@ public class DeviceUtils {
             ipAddress = null;
         }
         return ipAddress;
+    }*/
+
+    public InetAddress getDeviceIPAddress(Context context) {
+        InetAddress found_bcast_address = null;
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        try {
+            Enumeration<NetworkInterface> niEnum = NetworkInterface.getNetworkInterfaces();
+            while (niEnum.hasMoreElements()) {
+                NetworkInterface ni = niEnum.nextElement();
+                if (!ni.isLoopback()) {
+                    for (InterfaceAddress interfaceAddress : ni.getInterfaceAddresses()) {
+                        found_bcast_address = interfaceAddress.getAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return found_bcast_address;
     }
+
+
 
     public List<FileMetadata> getFilesFromDevice() {
         List<FileMetadata> filesList = new ArrayList<>();
         File directory = new File(Environment.getExternalStorageDirectory(), UPLOAD_DIRECTORY);
         directory.setReadable(true);
         File[] files = directory.listFiles();
-        for (int i = 0; files != null && i < files.length; i++)
-        {
+        for (int i = 0; files != null && i < files.length; i++) {
             FileMetadata fileMetadata = new FileMetadata();
             fileMetadata.setFileName(files[i].getName());
             fileMetadata.setFileSize(files[i].length());
